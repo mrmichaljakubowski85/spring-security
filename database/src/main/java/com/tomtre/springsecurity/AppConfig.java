720cb6a9f1944bb90ac2548ade13f884df8ec28f
mrmichaljakubowski85 <mrmichaljakubowski85@gmail.com>
2019-02-18 22:35:10 +0100

    next

--- database/src/main/java/com/tomtre/springsecurity/AppConfig.java
+++ database/src/main/java/com/tomtre/springsecurity/AppConfig.java
@@ -1,16 +1,34 @@
 package com.tomtre.springsecurity;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.tomtre.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig {
+
+    private static final Logger logger = Logger.getLogger(AppConfig.class.getName());
+
+    private final Environment env;
+
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
@@ -22,5 +40,31 @@ public class AppConfig {
         return internalResourceViewResolver;
     }
 
+    @Bean
+    public DataSource dataSource() {
+        logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
 
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        try {
+            dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        dataSource.setUser(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.password"));
+
+        dataSource.setInitialPoolSize(getIntProperty(env.getProperty("connection.pool.initialPoolSize")));
+        dataSource.setMaxIdleTime(getIntProperty(env.getProperty("connection.pool.maxIdleTime")));
+        dataSource.setMinPoolSize(getIntProperty(env.getProperty("connection.pool.minPoolSize")));
+        dataSource.setMaxPoolSize(getIntProperty(env.getProperty("connection.pool.maxPoolSize")));
+
+        return dataSource;
+    }
+
+    private int getIntProperty(String propName) {
+        return Integer.parseInt(env.getProperty(propName));
+    }
 }
--- database/src/main/java/com/tomtre/springsecurity/SecurityConfig.java
+++ database/src/main/java/com/tomtre/springsecurity/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.tomtre.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("tom").password("test123").roles(SecurityRoles.EMPLOYEE))
-                .withUser(users.username("mary").password("test123").roles(SecurityRoles.MANAGER, SecurityRoles.EMPLOYEE))
-                .withUser(users.username("susan").password("test123").roles(SecurityRoles.ADMIN, SecurityRoles.EMPLOYEE));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     //https://www.baeldung.com/registration-with-spring-mvc-and-spring-security
