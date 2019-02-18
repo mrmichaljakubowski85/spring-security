cc45290de8ff2c7fedb8cd65e8b94ec6c8f0950f
tomtre <tomek.tre@gmail.com>
2019-02-18 08:43:09 +0100

    next

---
+++ src/main/java/com/tomtre/springsecurity/AppConfig.java
@@ -0,0 +1,26 @@
+package com.tomtre.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.tomtre.springsecurity")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
+
+
+}
